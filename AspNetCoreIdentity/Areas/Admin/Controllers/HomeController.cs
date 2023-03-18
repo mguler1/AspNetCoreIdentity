b40a5753947f5ff5176da5f184a2d49bf17dd5ca
Muhammed <Muhammed@DESKTOP-RKEDQOI>
2023-03-18 23:51:36 +0300

    User List eklendi

---
+++ AspNetCoreIdentity/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using AspNetCoreIdentity.Areas.Admin.Models;
+using AspNetCoreIdentity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreIdentity.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public HomeController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> UserList()
+        {
+            var userList = await _userManager.Users.ToListAsync();
+            var userViewListModel = userList.Select(x => new UserListViewModel()
+            {
+                UserId = x.Id,
+                UserMail = x.Email,
+                UserName = x.UserName
+            }).ToList();
+            return View(userViewListModel);
+        }
+    }
+}
---
+++ AspNetCoreIdentity/Areas/Admin/Models/UserListViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace AspNetCoreIdentity.Areas.Admin.Models
+{
+    public class UserListViewModel
+    {
+        public UserListViewModel()
+        {
+            
+        }
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserMail { get; set; }
+       
+    }
+}
