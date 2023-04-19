76390fd1c4e5e3a01aede07126ff0ce5642d1e6d
Muhammed <Muhammed@DESKTOP-RKEDQOI>
2023-04-19 21:12:16 +0300

    UserEdit işlemleri devam edecek

--- AspNetCoreIdentity/Models/AppUser.cs
+++ AspNetCoreIdentity/Models/AppUser.cs
@@ -4,5 +4,9 @@ namespace AspNetCoreIdentity.Models
 {
     public class AppUser:IdentityUser
     {
+        public string? City { get; set; }
+        public string? Picture { get; set; }
+        public DateTime? BirthdDay { get; set; }
+        public byte? Gender { get; set; }
     }
 }
