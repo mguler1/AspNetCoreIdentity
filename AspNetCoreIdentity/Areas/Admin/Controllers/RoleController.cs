e53211776fb822d09243e87822f138a24cb67cde
Muhammed <Muhammed@DESKTOP-RKEDQOI>
2023-04-28 11:51:58 +0300

    Rol Ekleme İşlemi Eklendi

---
+++ AspNetCoreIdentity/Areas/Admin/Controllers/RoleController.cs
@@ -0,0 +1,40 @@
+﻿using AspNetCoreIdentity.Areas.Admin.Models;
+using AspNetCoreIdentity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using AspNetCoreIdentity.Extensions;
+namespace AspNetCoreIdentity.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class RoleController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly RoleManager<AppRole> _roleManager;
+
+        public RoleController(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult RoleCreate()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task <IActionResult> RoleCreate(RoleCreateViewModel request)
+        {
+            var result = await _roleManager.CreateAsync(new AppRole() { Name = request.Name });
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelErrorList(result.Errors);
+                return View();
+            }
+          return RedirectToAction(nameof(RoleController.Index));
+        }
+    }
+}
---
+++ AspNetCoreIdentity/Areas/Admin/Models/RoleCreateViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreIdentity.Areas.Admin.Models
+{
+    public class RoleCreateViewModel
+    {
+        [Required(ErrorMessage = "Rol Adı Boş Geçilemez")]
+        [Display(Name = "Rol Adı :")]
+        public string Name { get; set; }
+    }
+}
